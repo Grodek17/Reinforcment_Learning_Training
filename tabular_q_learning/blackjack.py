44d555bbdfdc6e81b54265295c10c89c07653517
Mateusz <grodek7483@gmail.com>
2026-04-29 09:13:46 +0200

    added simple dictionary for storing Q values and states binded togeher

--- tabular_q_learning/blackjack.py
+++ tabular_q_learning/blackjack.py
@@ -3,4 +3,26 @@ import numpy as np
 
 #hyperparameters
 alpha = 0.3         #learning rate
-gamma = 0.99
+gamma = 0.99        #"importance of future"
+epsilon = 0.9       #random move probability
+
+Q = {}
+state = (15,10, False)
+
+if state not in Q:
+    Q[state] = np.zeros(2)
+
+Q[state] = [1,2]
+
+print(Q, "initial_state")
+
+state = (20,21, False)
+
+if state not in Q:
+    Q[state] = np.zeros(2)
+Q[state] = [2,-3]
+
+print(Q, "second_state")
+
+
+
